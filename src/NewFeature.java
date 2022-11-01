4609d0c1faff01da376c1632720a19a70f3a2360
Julius Lassen <julius5517@gmail.com>
2022-11-01 11:22:57 +0100

    NewFeature added

---
+++ src/NewFeature.java
@@ -0,0 +1,16 @@
+<<<<<<< Updated upstream
+public class NewFeature {
+
+=======
+
+public class NewFeature {
+>>>>>>> Stashed changes
+
+
+    @Override
+
+    @Override
+    public String toString() {
+        return "This is a new feature";
+    }
+}
